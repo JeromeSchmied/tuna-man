f316977bc7ec1e843953d8e96a44ff814164c0dd
csboo <csongiboo@gmai.com>
2024-10-17 22:22:53 +0200

    fix: fixed a little mistake, now the file is a path_buf

--- src/args.rs
+++ src/args.rs
@@ -1,8 +1,10 @@
+use std::path::PathBuf;
+
 use clap::Parser;
 
 #[derive(Parser)]
 pub struct Args {
     /// .csv file to read data from, using 'player, grade' syntax
     #[arg(short, long)]
-    pub file: Option<String>,
+    pub file: PathBuf,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::process::exit;
-
 use args::Args;
 use clap::Parser;
 
@@ -13,38 +11,40 @@ mod ui;
 fn main() -> std::io::Result<()> {
     let args = Args::parse();
 
-    if let Some(f_path) = args.file.as_deref() {
-        // let tables = vec![Table::default(); 4];
-        let mut tournament = Tournament::from_path(f_path)?;
-        let mut i = 0;
-        while !tournament.is_end() {
-            println!("\n\n\n\nRound {i}.\n--------\n\nWinner branch matches:\n");
-            for w_match in &tournament.winner_branch {
-                println!("    {w_match}");
-            }
-            println!("\n-----------------------------\n\nLosing branch matches:\n");
-            for l_match in &tournament.loser_branch {
-                println!("    {l_match}");
-            }
-            println!("\n-----------------------------\n\n");
-            tournament.play_next_round();
-            i += 1;
+    let f_path = &args.file;
+    // let tables = vec![Table::default(); 4];
+    let mut tournament = Tournament::from_path(f_path)?;
+    let mut i = 0;
+    while !tournament.is_end() {
+        println!("\n\n\n\nRound {i}.\n--------\n\nWinner branch matches:\n");
+        for w_match in &tournament.winner_branch {
+            println!("    {w_match}");
         }
-        println!("\nTournament ended in {i} rounds, Results:");
-        println!("\n\nPODIUM\n------\n");
-        println!("Winner: {}", tournament.knocked.0.pop().unwrap());
-        println!("Second place: {}", tournament.knocked.0.pop().unwrap());
-        println!("Third place: {}", tournament.knocked.0.pop().unwrap());
-        println!("\nrunner-ups\n");
-        for (place, player) in tournament.knocked.0.iter().rev().enumerate() {
-            println!("{}. place: {player}", place + 4);
+        println!("\n-----------------------------\n\nLosing branch matches:\n");
+        for l_match in &tournament.loser_branch {
+            println!("    {l_match}");
         }
-    } else {
-        // TODO proper error handling
-        // Handle the case where no file path is provided
-        eprintln!("Error: No file path provided.");
-        exit(1);
+        println!("\n-----------------------------\n\n");
+        tournament.play_next_round();
+        i += 1;
     }
+    println!("\nTournament ended in {i} rounds, Results:");
+    println!("\n\nPODIUM\n------\n");
+    println!("Winner: {}", tournament.knocked.0.pop().unwrap());
+    println!("Second place: {}", tournament.knocked.0.pop().unwrap());
+    println!("Third place: {}", tournament.knocked.0.pop().unwrap());
+    println!("\nrunner-ups\n");
+    for (place, player) in tournament.knocked.0.iter().rev().enumerate() {
+        println!("{}. place: {player}", place + 4);
+    }
+
+    // else {
+    //     // TODO proper error handling
+    //     // Handle the case where no file path is provided
+    //     eprintln!("Error: No file path provided.");
+    //     exit(1);
+    // }
+
     // players.save();
 
     Ok(())
