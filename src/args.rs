7274a5b198c3cdbac2b22580242c05bdf2ad68b7
csboo <csongiboo@gmai.com>
2024-10-17 21:04:59 +0200

    misc: more help info

--- src/args.rs
+++ src/args.rs
@@ -2,6 +2,7 @@ use clap::Parser;
 
 #[derive(Parser)]
 pub struct Args {
+    /// .csv file to read data from, using 'player, grade' syntax
     #[arg(short, long)]
     pub file: Option<String>,
 }
