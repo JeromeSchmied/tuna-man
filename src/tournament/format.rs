bf1d25a304090e1dd0addb744d5e4364ecacadc5
Jeromos Kovács <iitsnotme214@proton.me>
2024-11-06 23:20:18 +0100

    feat(formats): added round-robin support

--- src/tournament/format.rs
+++ src/tournament/format.rs
@@ -1,4 +1,9 @@
-use super::{backend::Backend, players::Players, structs::Duel};
+use super::{
+    backend::Backend,
+    players::Players,
+    structs::{Duel, Player},
+};
+use std::collections::HashMap;
 
 #[derive(Debug, Clone, PartialEq, Eq, clap::ValueEnum)]
 pub enum Supported {
@@ -252,31 +257,130 @@ impl<B: Backend> Format<B> for SingleElemination {
 
 #[derive(Default, PartialEq, Eq, Clone, Debug)]
 /// implemented according to wikipedia <https://en.wikipedia.org/wiki/Round-robin_tournament>
-pub struct RoundRobin {}
+pub struct RoundRobin {
+    /// currently relevant [`Duel`]s to be carried out
+    duels: Vec<Duel>,
+    /// all the participating [`Players`]
+    players: Players,
+    /// points of the `players`
+    points: HashMap<Player, u8>,
+    /// the number of `round`s already executed
+    round: usize,
+}
 impl RoundRobin {
-    //     pub fn new(branch: Vec<Duel>, knocked: Players) -> Self {
-    //         Self { branch, knocked }
-    //     }
+    /// number of [`Self::players`]
+    fn len(&self) -> usize {
+        self.players.0.len()
+    }
+    /// update the [`Self::duels`], so in the upcoming round [`Player`]s play against other ones as well
+    /// circle-method, implemented according to wikipedia <https://en.wikipedia.org/wiki/Round-robin_tournament#Circle_method>
+    fn update_duels(&mut self) {
+        // the indexed order of duels
+        let mut duel_idxs = (1..self.len()).collect::<Vec<_>>();
+        duel_idxs.rotate_right(self.round);
+        duel_idxs.insert(0, 0);
+
+        let mut duels = vec![];
+        while !duel_idxs.is_empty() {
+            // index of the first one
+            let homie = duel_idxs.remove(0);
+            // index of the last one
+            let guest = duel_idxs.pop().unwrap();
+            // the actual players themselves
+            let (homie, guest) = (
+                self.players.0.get(homie).unwrap().clone(),
+                self.players.0.get(guest).unwrap().clone(),
+            );
+            duels.push(Duel::new(homie, guest));
+        }
+        // apply changes
+        self.duels = duels;
+    }
+    pub fn new(
+        duels: Vec<Duel>,
+        players: Players,
+        points: HashMap<Player, u8>,
+        round: usize,
+    ) -> Self {
+        Self {
+            duels,
+            players,
+            points,
+            round,
+        }
+    }
 }
 impl<B: Backend> Format<B> for RoundRobin {
     fn add_players(&mut self, players: Players) {
-        todo!()
+        // simply apply players
+        self.players = players;
+
+        // odd number of players
+        if self.len() % 2 == 1 {
+            // add ghost player
+            self.players.0.push(Player::default());
+        }
+
+        // set every player's points to 0
+        let points = self
+            .players
+            .0
+            .clone()
+            .into_iter()
+            .map(|p| (p, 0u8))
+            .collect::<HashMap<_, _>>();
+        // apply points
+        self.points = points;
+
+        // init duels
+        self.update_duels();
     }
 
     fn is_end(&self) -> bool {
-        todo!()
+        // every player played against every player
+        self.round == self.len()
     }
 
     fn play_round(&mut self) {
-        todo!()
+        // execute duels: get outcomes
+        for duel in &self.duels {
+            println!("\n{duel}");
+            // ignore duel if any players are ghosts
+            if duel.homie.is_unset() || duel.guest.is_unset() {
+                continue;
+            }
+            // execute duel
+            let (winner, _loser) = duel.clone().play(B::get_outcome);
+            // winner get's a point
+            self.points.entry(winner).and_modify(|p| *p += 1);
+        }
+        // update the duels for the upcoming round
+        self.update_duels();
+        // another round is executed
+        self.round += 1;
     }
 
     fn print_status(&self) {
-        todo!()
+        println!("\n\nPOINTS:\n");
+        for player in &self.players.0 {
+            println!("    {player}: {}", self.points[player]);
+        }
+        println!("\n\n-------------------\n\nDUELS:\n");
+        for duel in &self.duels {
+            println!("    {duel}");
+        }
+        println!("\n\n\n");
     }
 
     fn results(self) -> Players {
-        todo!()
+        // hashmap -> vec
+        let mut result: Vec<_> = self.points.into_iter().collect();
+        // sorted by points
+        result.sort_by(|x, y| x.1.cmp(&y.1));
+        // extract players
+        let result: Vec<_> = result.into_iter().map(|(player, _points)| player).collect();
+        // into Players
+        Players(result)
     }
 }
 
